784bedf89786581cf9cbfb9cd03f5c82fb271841
Vlad Shashkov <shashkov.root@gmail.com>
2024-05-25 22:19:50 +0700

    Added autoload for stats-analysis

---
+++ src/CPLicense.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+final class CPLicense
+{
+	/**
+	 * @return non-empty-string
+	 */
+	public function CompanyName(): string
+	{
+
+	}
+
+
+	public function IsValid(): bool
+	{
+
+	}
+
+
+
+	public function Type(): string
+	{
+
+	}
+
+}
